d0cef15eca899263043e000b74a15bb78c83f4fe
cold <1812821235@qq.com>
2020-06-22 08:30:43 +0800

    Map

---
+++ datastruct/src/main/java/com/cold/datastruct/d7_setandmap/map/Map.java
@@ -0,0 +1,12 @@
+package com.cold.datastruct.d7_setandmap.map;
+
+public interface Map<K, V> {
+
+    void add(K key, V value);
+    V remove(K key);
+    boolean contains(K key);
+    V get(K key);
+    void set(K key, V newValue);
+    int getSize();
+    boolean isEmpty();
+}
